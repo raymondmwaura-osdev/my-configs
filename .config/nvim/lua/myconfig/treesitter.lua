e1dc875a57f18a662ba590f86cfa65805dfca699
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2025-12-10 22:35:43 +0300

    Add javascript parser.

--- .config/nvim/lua/myconfig/treesitter.lua
+++ .config/nvim/lua/myconfig/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
- ensure_installed = { "lua", "sway", "python", "markdown", "bash", "toml"},
+ ensure_installed = { "lua", "sway", "python", "markdown", "bash", "toml", "javascript"},
  sync_install = true,
  auto_install = false,
  highlight = {
