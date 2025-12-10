1545bd412d1c0fb6e9b92ffff958155bcf2d0fbf
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2025-12-10 22:35:25 +0300

    Make nvim have the same transparency as the terminal emulator (alacritty).

--- .config/nvim/lua/myconfig/init.lua
+++ .config/nvim/lua/myconfig/init.lua
@@ -1,4 +1,4 @@
-require("myconfig.options")
 require("myconfig.keymaps")
 require("myconfig.lazy")
 require("myconfig.treesitter")
+require("myconfig.options")
--- .config/nvim/lua/myconfig/options.lua
+++ .config/nvim/lua/myconfig/options.lua
@@ -4,3 +4,11 @@ vim.o.expandtab = true
 
 vim.o.number = true
 vim.o.relativenumber = true
+
+vim.opt.termguicolors = true
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+
