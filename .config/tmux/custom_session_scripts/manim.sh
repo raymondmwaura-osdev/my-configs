3421a7aa0e334b61c79eb54282ea797c1781a46f
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-06 15:49:27 +0300

    Create 2 more windows: docs and code.

--- .config/tmux/custom_session_scripts/manim.sh
+++ .config/tmux/custom_session_scripts/manim.sh
@@ -6,6 +6,8 @@ keystash has -t manim 2> /dev/null
 if [[ $? != 0 ]]; then
     tmux new-session -d -s $SESSION -c $MANIM_DIR -n run
     tmux send-keys -t ${SESSION}:run "source venv/bin/activate" Enter
+    tmux new-window -d -t $SESSION -c $MANIM_DIR -n docs nvim
+    tmux new-window -d -t $SESSION -c $MANIM_DIR -n code nvim
 fi
 
 tmux attach -t $SESSION -c ~ # Use `-c ~` to reset the directory where new windows will be opened.
