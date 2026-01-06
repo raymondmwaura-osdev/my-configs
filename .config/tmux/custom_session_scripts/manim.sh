137db4585fb034beaf447897239f86bfb65b96bb
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-06 15:36:18 +0300

    Add manim session script.

---
+++ .config/tmux/custom_session_scripts/manim.sh
@@ -0,0 +1,11 @@
+# Set up tmux environment for learning manim.
+SESSION=manim
+MANIM_DIR=/mnt/data/python-notes-and-insights/manim
+
+keystash has -t manim 2> /dev/null
+if [[ $? != 0 ]]; then
+    tmux new-session -d -s $SESSION -c $MANIM_DIR -n run
+    tmux send-keys -t ${SESSION}:run "source venv/bin/activate" Enter
+fi
+
+tmux attach -t $SESSION -c ~ # Use `-c ~` to reset the directory where new windows will be opened.
