f7fe18c88d180c02ea3d010dbad69a5aef9054b4
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-13 20:40:36 +0300

    auth_sys.sh: Add setup script for the authentication system project.

---
+++ .config/tmux/custom_session_scripts/auth_sys.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This shell scripts adds windows to the "web_dev" session. These new
+# windows are used when working on the authentication system project.
+# New windows:
+#   1. 'auth_sys': A terminal opened in the directory containing the authentication system project.
+#   2. 'code_auth_sys`: An nvim session opened in the authentication system project directory.
+
+PROJECT_DIR=/mnt/data/authentication_system
+
+# Create main web dev session.
+cd "$(dirname $0)"
+source web_dev.sh 0 # 'source' to be able to access its variables.
+
+# Add windows.
+tmux_has_window() {
+    # Check if the tmux session $SESSION contains the specified window.
+    # The name of the window is expected to be in $1.
+    if tmux list-windows -t $SESSION \
+        -F "#{window_name}" \
+        | grep -w $1 > /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if ! tmux_has_window auth_sys; then
+    tmux new-window -d -t $SESSION -n auth_sys -c $PROJECT_DIR
+fi
+
+if ! tmux_has_window code_auth_sys; then
+    tmux new-window -d -t $SESSION -n code_auth_sys -c $PROJECT_DIR nvim
+fi
+
+tmux attach -t $SESSION -c ~
