c042f71aa8022201c008e92e30c94518a63c9ee2
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-26 14:58:44 +0300

    Add tmux setup script for ringzeroacademy work.

---
+++ .config/tmux/custom_session_scripts/ringzeroacademy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SESSION=ringzeroacademy
+WORKING_DIR=/mnt/data/RingZeroAcademy/ringzeroacademy/design/tutorial_page
+
+if ! tmux has-session -t $SESSION 2> /dev/null ; then
+    tmux new -d -s $SESSION -c $WORKING_DIR
+    tmux new-window -d -t $SESSION -c $WORKING_DIR nvim
+fi
+
+tmux attach -t $SESSION
