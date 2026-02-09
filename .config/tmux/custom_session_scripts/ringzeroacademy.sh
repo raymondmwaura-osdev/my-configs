0a87bb3e9433da675b5ffdc442b2776f1b11a78e
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-02-09 16:01:12 +0300

    Add path to design directory.

--- .config/tmux/custom_session_scripts/ringzeroacademy.sh
+++ .config/tmux/custom_session_scripts/ringzeroacademy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 SESSION=ringzeroacademy
-WORKING_DIR=/mnt/data/RingZeroAcademy/ringzeroacademy/design/tutorial_page
+DESIGN_DIR=/mnt/data/RingZeroAcademy/ringzeroacademy/design
+TUTORIAL_PAGE_DIR=${DESIGN_DIR}/tutorial_page
+WORKING_DIR=$DESIGN_DIR
 
 if ! tmux has-session -t $SESSION 2> /dev/null ; then
     tmux new -d -s $SESSION -c $WORKING_DIR
