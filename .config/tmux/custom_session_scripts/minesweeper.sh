590cc21e508d8f1d7fb2707742fb0401c0e43277
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-04-06 12:33:09 +0300

    Add a terminal window and a nvim window for the static directory.

--- .config/tmux/custom_session_scripts/minesweeper.sh
+++ .config/tmux/custom_session_scripts/minesweeper.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 WORKING_DIR=/mnt/data/minesweeper
+STATIC_DIR=${WORKING_DIR}/static/main
 BACKEND_DIR=${WORKING_DIR}/fullstack/backend
 FRONTEND_DIR=${WORKING_DIR}/fullstack/frontend
 SESSION=minesweeper
 if ! tmux has-session -t $SESSION 2> /dev/null; then
     tmux new -d -n general -s $SESSION -c $WORKING_DIR
     tmux new-window -d -n general_nvim -t $SESSION -c $WORKING_DIR nvim
+    tmux new-window -d -n static -t $SESSION -c $STATIC_DIR
+    tmux new-window -d -n nvim_static -t $SESSION -c $STATIC_DIR nvim
     tmux new-window -d -n backend -t $SESSION -c $BACKEND_DIR
     tmux new-window -d -n nvim_backend -t $SESSION -c $BACKEND_DIR nvim
     tmux new-window -d -n frontend -t $SESSION -c $FRONTEND_DIR
