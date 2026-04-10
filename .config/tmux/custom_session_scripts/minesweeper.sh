0d9bdf791ebdc973a8bfff0f56d08f444c3d7444
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-04-10 20:11:47 +0300

    minesweeper.sh: Remove the backend and frontend windows, and split the static window for the server.

--- .config/tmux/custom_session_scripts/minesweeper.sh
+++ .config/tmux/custom_session_scripts/minesweeper.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 WORKING_DIR=/mnt/data/minesweeper
 STATIC_DIR=${WORKING_DIR}/static/main
-BACKEND_DIR=${WORKING_DIR}/fullstack/backend
-FRONTEND_DIR=${WORKING_DIR}/fullstack/frontend
+#BACKEND_DIR=${WORKING_DIR}/fullstack/backend
+#FRONTEND_DIR=${WORKING_DIR}/fullstack/frontend
 SESSION=minesweeper
 if ! tmux has-session -t $SESSION 2> /dev/null; then
     tmux new -d -n general -s $SESSION -c $WORKING_DIR
-    tmux new-window -d -n general_nvim -t $SESSION -c $WORKING_DIR nvim
     tmux new-window -d -n static -t $SESSION -c $STATIC_DIR
+    tmux split-window -d -h -t ${SESSION}:static -c $STATIC_DIR -l 20% \
+        python -m http.server
     tmux new-window -d -n nvim_static -t $SESSION -c $STATIC_DIR nvim
-    tmux new-window -d -n backend -t $SESSION -c $BACKEND_DIR
-    tmux new-window -d -n nvim_backend -t $SESSION -c $BACKEND_DIR nvim
-    tmux new-window -d -n frontend -t $SESSION -c $FRONTEND_DIR
-    tmux new-window -d -n nvim_frontend -t $SESSION -c $FRONTEND_DIR nvim
+#    tmux new-window -d -n backend -t $SESSION -c $BACKEND_DIR
+#    tmux new-window -d -n nvim_backend -t $SESSION -c $BACKEND_DIR nvim
+#    tmux new-window -d -n frontend -t $SESSION -c $FRONTEND_DIR
+#    tmux new-window -d -n nvim_frontend -t $SESSION -c $FRONTEND_DIR nvim
 fi
 
 tmux attach -t $SESSION -c ~
