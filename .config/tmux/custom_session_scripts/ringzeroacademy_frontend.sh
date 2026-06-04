3f3cfa9e7904682fc4ca74ad37389092829df24c
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-06-04 22:42:35 +0300

    Make custom tmux setup script for ringzeroacademy frontend work.

---
+++ .config/tmux/custom_session_scripts/ringzeroacademy_frontend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SESSION=ringzeroacademy_frontend
+REPO_DIR=/mnt/data/RingZeroAcademy/ringzeroacademy_frontend
+
+if ! tmux has-session -t $SESSION 2> /dev/null ; then
+    tmux new -d -s $SESSION -c $REPO_DIR -n editor nvim
+    tmux new-window -d -t $SESSION -c $REPO_DIR -n server
+    tmux send-keys -t ${SESSION}:server "npm run dev" Enter
+    tmux new-window -d -t $SESSION -c $REPO_DIR -n terminal
+fi
+
+tmux attach -t $SESSION -c ~
