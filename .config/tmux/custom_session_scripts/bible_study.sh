1edd3b2085ede71e6c6945e7827cb84ee68e5aba
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-06-21 21:57:19 +0300

    Open the Exodus directory instead of the Genesis directory.

--- .config/tmux/custom_session_scripts/bible_study.sh
+++ .config/tmux/custom_session_scripts/bible_study.sh
@@ -2,11 +2,14 @@
 
 SESSION=bible_study
 BIBLE_STUDY_DIR=/mnt/data/bible-study
+
 GENESIS_DIR=/mnt/data/bible-study/books/01_Genesis
+EXODUS_DIR=/mnt/data/bible-study/books/02_Exodus
+CURRENT_BOOK_DIR=$EXODUS_DIR
 
 if ! tmux has-session -t $SESSION 2> /dev/null; then
     tmux new-session -d -s $SESSION -n git -c $BIBLE_STUDY_DIR
-    tmux new-window -d -n nvim -t $SESSION -c $GENESIS_DIR nvim
+    tmux new-window -d -n nvim -t $SESSION -c $CURRENT_BOOK_DIR nvim
 fi
 
 tmux attach -t $SESSION -c ~
